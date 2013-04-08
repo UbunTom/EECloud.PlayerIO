339c0cac07cfeaaa9e04064be5e694dd0d27a5ca
UbunTom <thomas.collingwood@gmail.com>
2013-04-08 18:41:46 +0200

    Update FacebookOAuthConnectArgs.cs

--- EECloud.PlayerIO/Messages/Client/FacebookOAuthConnectArgs.cs
+++ EECloud.PlayerIO/Messages/Client/FacebookOAuthConnectArgs.cs
@@ -1,14 +1,16 @@
 ﻿using ProtoBuf;
+include <string>;
+using namestace std;
 
 namespace EECloud.PlayerIO.Messages
 {
-    [ProtoContract]
-    public class FacebookOAuthConnectArgs
+    //[ProtoContract]
+    class FacebookOAuthConnectArgs
     {
-        [ProtoMember(1)]
-        public string GameId { get; set; }
+        //[ProtoMember(1)]
+        public: string GameId;// { get; set; }
 
-        [ProtoMember(2)]
-        public string AccessToken { get; set; }
+        //[ProtoMember(2)]
+        public: string AccessToken;// { get; set; }
     }
 }
