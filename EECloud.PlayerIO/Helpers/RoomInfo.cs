fa7df51d5a6a15aa033f5b811874837ec5dff4f1
UbunTom <thomas.collingwood@gmail.com>
2013-04-08 17:31:58 +0200

    Update RoomInfo.cs

--- EECloud.PlayerIO/Helpers/RoomInfo.cs
+++ EECloud.PlayerIO/Helpers/RoomInfo.cs
@@ -1,32 +1,40 @@
-﻿using System.Collections.Generic;
+//Fix includes
+using System.Collections.Generic;
 using ProtoBuf;
+include <map>
+include <string>
+using namespace std;
 
 namespace EECloud.PlayerIO
 {
-    [ProtoContract]
-    public class RoomInfo
-    {
-        [ProtoMember(1)]
-        public string Id { get; private set; }
-
-        [ProtoMember(2)]
-        public string RoomType { get; private set; }
-
-        [ProtoMember(3)]
-        public int OnlineUsers { get; private set; }
-
-        /// <summary>
-        /// The current room data for the room.
-        /// </summary>
-        [ProtoMember(4)]
-        public Dictionary<string, string> RoomData { get; private set; }
-
-        internal RoomInfo(string id, string roomType, int onlineUsers, Dictionary<string, string> roomData)
-        {
-            Id = id;
-            RoomType = roomType;
-            OnlineUsers = onlineUsers;
-            RoomData = (roomData ?? new Dictionary<string, string>());
-        }
-    }
-} 
+	//[ProtoContract]
+	class RoomInfo
+	{
+		//[ProtoMember(1)]
+		private: string Id; //{ get; private set; }
+		public: string getId(){return Id;}
+		
+		//[ProtoMember(2)]
+		private: string RoomType; //{ get; private set; }
+		public: string getRoomType(){return RoomType;}
+		
+		//[ProtoMember(3)]
+		private: int OnlineUsers; //{ get; private set; }
+		public: int getOnlineUsers(){return OnlineUsers;}	
+		
+		/// <summary>
+		/// The current room data for the room.
+		/// </summary>
+		//[ProtoMember(4)]
+		private map<string, string> RoomData; //{ get; private set; }
+		public: map<string, string> getRoomData(){return RoomData;}
+		
+		void RoomInfo(string id, string roomType, int onlineUsers, map<string, string> roomData)
+		{
+		    Id = id;
+		    RoomType = roomType;
+		    OnlineUsers = onlineUsers;
+		    RoomData = roomData;//In C++ a map can't be null (roomData != null ? roomData : new map<string, string>());
+		}
+	}
+}
