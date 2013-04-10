732fd7509eff4d020816aefa4806c6567aed6e4d
UbunTom <thomas.collingwood@gmail.com>
2013-04-10 22:47:29 +0200

    Update PlayerIORegistrationError.cs

--- EECloud.PlayerIO/Error/PlayerIORegistrationError.cs
+++ EECloud.PlayerIO/Error/PlayerIORegistrationError.cs
@@ -3,14 +3,14 @@
     /// <summary>
     /// If there are any errors when using the SimpleRegister method of QuickConnect, you'll get back an error object of this type that holds more details about the cause of the error. You can use this information to provide better help for your users when they are filling out your registration form.
     /// </summary>
-    public class PlayerIORegistrationError : PlayerIOError
+    public class PlayerIORegistrationError : public PlayerIOError
     {
         public string CaptchaError { get; private set; }
         public string EmailError { get; private set; }
         public string PasswordError { get; private set; }
         public string UsernameError { get; private set; }
 
-        public PlayerIORegistrationError(ErrorCode errorCode, string message, string usernameError, string passwordError, string emailError, string captchaError) : base(errorCode, message)
+        public PlayerIORegistrationError(ErrorCode errorCode, string message, string usernameError, string passwordError, string emailError, string captchaError) : PlayerIOError(errorCode, message)
         {
             UsernameError = usernameError;
             PasswordError = passwordError;
