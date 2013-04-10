4fb152b88c1573f333e91f94c7e83fa0ea3037df
UbunTom <thomas.collingwood@gmail.com>
2013-04-10 22:38:07 +0200

    Update PlayerIOError.cs

--- EECloud.PlayerIO/Error/PlayerIOError.cs
+++ EECloud.PlayerIO/Error/PlayerIOError.cs
@@ -1,20 +1,25 @@
-﻿using System;
+#include <exception>
 
 namespace EECloud.PlayerIO
 {
     /// <summary>
     /// The base class for catching and reporting errors related to the Player.IO client.
     /// </summary>
-    public class PlayerIOError : ApplicationException
+    public class PlayerIOError: public std::exception
     {
-        public ErrorCode ErrorCode { get; private set; }
+        public: ErrorCode ErrorCode;
+        
+        public: string message
+        
+        public: const char* what(){return message.c_str();}
+        public: string what(){return message;}
 
         /// <summary>
         /// Creates a new instance of PlayerIOError.
         /// </summary>
         /// <param name="errorCode">The code of the error that happened.</param>
         /// <param name="message">The error explained by words.</param>
-        public PlayerIOError(ErrorCode errorCode, string message) : base(message)
+        public: PlayerIOError(ErrorCode errorCode, string _message) : message(_message)
         {
             ErrorCode = errorCode;
         }
