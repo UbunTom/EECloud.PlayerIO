7063f8933b974768ac38222e0f5b7ec0ff6c1e90
UbunTom <thomas.collingwood@gmail.com>
2013-04-10 20:54:36 +0200

    Update MessageType.hpp

--- EECloud.PlayerIO/Enums/MessageType.hpp
+++ EECloud.PlayerIO/Enums/MessageType.hpp
@@ -2,9 +2,9 @@
 
 namespace EECloud.PlayerIO
 {
-    enum MessageType
-    {
-        Integer = 0,
+	enum MessageType
+	{
+		Integer = 0,
 		UInteger = 1,
 		Long = 2,
 		ULong = 3,
@@ -13,30 +13,29 @@ namespace EECloud.PlayerIO
 		String = 6,
 		ByteArray = 7,
 		Boolean = 8
-    }
+	}
     
-    class MessageObject
-    {
-    	int _MessageType
-    };
+	class MessageObject
+	{
+		int _MessageType
+	};
     
-    class _Integer: public MessageObject
-    {int data;};
-     class _UInteger: public MessageObject
-    {undigen int data;};
-     class _Long: public MessageObject
-    {long data;};
-     class _ULong: public MessageObject
-    {unsigned long data;};
-     class _Double: public MessageObject
-    {double data;};
-     class _Float: public MessageObject
-    {float data;};
-     class _String: public MessageObject
-    {string data;};
-     class _ByteArray: public MessageObject
-    {unsigned int size; unsigned char* data;};
-     class _Boolean: public MessageObject
-    {bool data;};
-
+	class _Integer: public MessageObject
+		{int data;};
+	class _UInteger: public MessageObject
+		{unsigned int data;};
+	class _Long: public MessageObject
+		{long data;};
+	class _ULong: public MessageObject
+		{unsigned long data;};
+	class _Double: public MessageObject
+		{double data;};
+	class _Float: public MessageObject
+		{float data;};
+	class _String: public MessageObject
+		{string data;};
+	class _ByteArray: public MessageObject
+		{unsigned int size; unsigned char* data;};
+	class _Boolean: public MessageObject
+		{bool data;};
 }
