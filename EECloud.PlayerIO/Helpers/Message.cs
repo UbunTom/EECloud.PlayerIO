10306af66dc839167c3d532cc488719374e1183b
UbunTom <thomas.collingwood@gmail.com>
2013-04-10 21:39:26 +0200

    Update Message.cs

--- EECloud.PlayerIO/Helpers/Message.cs
+++ EECloud.PlayerIO/Helpers/Message.cs
@@ -5,135 +5,96 @@ using System.Linq;
 
 namespace EECloud.PlayerIO
 {
+    
     /// <summary>
     /// Represents a message sent between the client and the server.
     /// <para>A message consists of a type (string), and zero or more parameters that are supported.</para>
     /// </summary>
-    public class Message : IEnumerable<object>
+    public class Message
     {
         /// <summary>Creates a new Message.</summary>
         /// <param name="type">The type of message to create.</param>
         /// <param name="parameters">A list of the data to add to the message.</param>
-        public Message(string type, params object[] parameters)
+        public: Message(string type, List<Object*>)
         {
             Type = type;
             Add(parameters);
         }
 
-        public string Type { get; private set; }
-        private readonly List<Tuple<object, MessageType>> _parameters = new List<Tuple<object, MessageType>>();
+        public: string Type;
+        private: List<MessageObject*> _parameters;
 
-        public int Count
+        public: int Count
         {
-            get { return _parameters.Count; }
-        }
-
-        #region Get
-        public object Item(int index)
-        {
-            return _parameters[index].Item1;
+            return _parameters.Count.size;
         }
 
-        public string GetString(int index)
+        public: void* Item(int index)
         {
-            return (string)_parameters[index].Item1;
+            return _parameters[index].data;
         }
-        public int GetInteger(int index)
-        {
-            return (int)_parameters[index].Item1;
-        }
-        public uint GetUInteger(int index)
-        {
-            return (uint)_parameters[index].Item1;
-        }
-        public long GetLong(int index)
-        {
-            return (long)_parameters[index].Item1;
-        }
-        public ulong GetULong(int index)
-        {
-            return (ulong)_parameters[index].Item1;
-        }
-        public byte[] GetByteArray(int index)
-        {
-            return (byte[])_parameters[index].Item1;
-        }
-        public float GetFloat(int index)
-        {
-            return (float)_parameters[index].Item1;
-        }
-        public double GetDouble(int index)
-        {
-            return (double)_parameters[index].Item1;
-        }
-        public bool GetBoolean(int index)
-        {
-            return (bool)_parameters[index].Item1;
-        }
-        #endregion
 
-        #region Add
-        private const string ParameterNullText = "Can't add null values to Player.IO Messages.";
-
-        public void Add(object obj)
+        public: string GetString(int index)
         {
-            if (obj == null)
+            return (string)(*_parameters[index].data);
+        }
+        public: int GetInteger(int index)
+        {
+            return (int)(*_parameters[index].data);
+        }
+        public: uint GetUInteger(int index)
+        {
+            return (uint)(*_parameters[index].data);
+        }
+        public: long GetLong(int index)
+        {
+            return (long)(*_parameters[index].data);
+        }
+        public: ulong GetULong(int index)
+        {
+            return (ulong)(*_parameters[index].data);
+        }
+        public: byte[] GetByteArray(int index)
+        {
+            return (byte[])(*_parameters[index].data);
+        }
+        public: float GetFloat(int index)
+        {
+            return (float)(*_parameters[index].data);
+        }
+        public: double GetDouble(int index)
+        {
+            return (double)(*_parameters[index].data);
+        }
+        public: bool GetBoolean(int index)
+        {
+            return (bool)(*_parameters[index].data);
+        }
+
+        private: const string ParameterNullText = "Can't add null values to Player.IO Messages.";
+
+        public: void Add(TypeTuple obj)
+        {
+            if (obj.data == null)
             {
                 throw new ArgumentNullException(ParameterNullText);
             }
-            if (obj is string)
-            {
-                _parameters.Add(Tuple.Create(obj, MessageType.String));
-            }
-            else if (obj is int)
-            {
-                _parameters.Add(Tuple.Create(obj, MessageType.Integer));
-            }
-            else if (obj is bool)
-            {
-                _parameters.Add(Tuple.Create(obj, MessageType.Boolean));
-            }
-            else if (obj is float)
-            {
-                _parameters.Add(Tuple.Create(obj, MessageType.Float));
-            }
-            else if (obj is double)
-            {
-                _parameters.Add(Tuple.Create(obj, MessageType.Double));
-            }
-            else if (obj is byte[])
-            {
-                _parameters.Add(Tuple.Create(obj, MessageType.ByteArray));
-            }
-            else if (obj is uint)
-            {
-                _parameters.Add(Tuple.Create(obj, MessageType.UInteger));
-            }
-            else if (obj is long)
-            {
-                _parameters.Add(Tuple.Create(obj, MessageType.Long));
-            }
-            else if (obj is ulong)
-            {
-                _parameters.Add(Tuple.Create(obj, MessageType.ULong));
-            }
-            else
-            {
-                throw new InvalidOperationException(
+  
+            _parameters.push_back(obj);
+            
+             /*   throw new InvalidOperationException(
                     "Player.IO Messages only support objects of types: String, Integer, Boolean, Float, Double, Byte[], UInteger, Long & ULong." + Environment.NewLine +
-                    "Type '" + obj.GetType().FullName + "' is not supported.");
-            }
+                    "Type '" + obj.GetType().FullName + "' is not supported.");*/
+            
         }
 
-        public void Add(params object[] parameters)
+        public: void Add(vector<TypeTuple> pararameters)
         {
-            foreach (var obj in parameters)
-            {
-                Add(obj);
+            for(int i=0;i<parameters.size;i++)
+                Add(parameters[i]);
             }
         }
 
-        #region Separate methods for allowed object types
         public void Add(string parameter)
         {
             if (parameter == null)
@@ -144,6 +105,7 @@ namespace EECloud.PlayerIO
         }
         public void Add(int parameter)
         {
+            
             _parameters.Add(Tuple.Create((object)parameter, MessageType.Integer));
         }
         public void Add(uint parameter)
@@ -178,14 +140,13 @@ namespace EECloud.PlayerIO
         {
             _parameters.Add(Tuple.Create((object)parameter, MessageType.Boolean));
         }
-        #endregion
-        #endregion
 
-        public override string ToString()
+
+        public: string ToString()
         {
-            string output = "Message.Type = " + Type + Environment.NewLine +
+            string output = "Message.Type = " + Type + "\n" +
                 "Message.Parameters";
-            for (var i = 0; i < _parameters.Count; i++)
+            for (var i = 0; i < _parameters.size; i++)
             {
                 output += Environment.NewLine +
                     "  [" + i + "] (" + _parameters[i].Item1.GetType().Name + ") = " +
@@ -194,14 +155,6 @@ namespace EECloud.PlayerIO
             return output;
         }
 
-        public IEnumerator<object> GetEnumerator()
-        {
-            return _parameters.Select(t => t.Item1).GetEnumerator();
-        }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
     }
 }
