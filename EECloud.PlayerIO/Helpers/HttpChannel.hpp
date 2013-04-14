d0b7f6845fe8e7b161d3ad08aa15bb21bf520aeb
UbunTom <thomas.collingwood@gmail.com>
2013-04-14 16:06:40 +0200

    Create HttpChannel.hpp

---
+++ EECloud.PlayerIO/Helpers/HttpChannel.hpp
@@ -0,0 +1,85 @@
+namespace EECloud
+{
+	class CURLRequest
+	{
+		private: CURL* handle;
+		public: struct curl_slist *headerlist;
+
+		public: CURLRequest();
+		public: ~CURLRequest();
+
+		public: CURL* handle(){return handle;}
+
+	}
+
+	size_t write_data(void* ptr, size_t size, size_t nmemb, void* stream);
+
+	class HttpChannel
+	{
+		private: const string EndpointUri = "http://api.playerio.com/api";
+		private: map<string, string> _headers;
+
+		public:
+		template<class TRequest, class TResponse, class TError>
+		TResponse Request(int method, TRequest args)// where TError : Exception
+        {
+			TResponse r = TResponse();//default(TResponse);
+			CURLRequest request = GetRequest(method);
+
+			stringstream protobufStream;//Prepare data to send
+			args->SerializeToOstream(&protobufStream);
+			string protobufString = protobufStream.str();
+			curl_easy_setopt(request.handle(), CURLOPT_POSTFIELDS, protobufString);
+       			curl_easy_setopt(request.handle(), CURLOPT_POSTFIELDSIZE, protobufString.length());
+       			
+       			std::stringstream response;//Prepare to recieve data
+       			std::stringstream headers;
+       			curl_easy_setopt(request.handle(), CURLOPT_WRITEFUNCTION, write_data);
+    			curl_easy_setopt(request.handle(), CURLOPT_WRITEDATA, &response);
+    			curl_easy_setopt(curl, CURLOPT_WRITEHEADER, &headers);
+
+			try
+			{
+				res = curl_easy_perform(curl);	
+
+				if (res!=0)
+				{
+					stringstream errstream;
+					errstream << res;
+					throw PlayerIOError(ErrorCode.GeneralError, "A cURL error occured: " + errstream.str());
+				}
+
+				if (!r.ParseFromIstream(&response)) {
+					throw PlayerIOError(ErrorCode.GeneralError, "Failed to parse web response");
+				}
+				/*using (var responseStream = request.GetResponse().GetResponseStream())
+				{
+					if (ReadHeader(responseStream))
+					{
+						r = Serializer.Deserialize<TResponse>(responseStream);
+					}
+					else
+					{
+						throw GetError<TError>(responseStream);
+					}
+				}*/
+			}
+			catch (PlayerIOError err)
+			{
+			/*	if (webException.Response == null)
+				{
+					throw new PlayerIOError(ErrorCode.GeneralError, "Connection to the Player.IO WebService has just been unexpectedly terminated.");
+				}
+			    
+			    using (var stream = webException.Response.GetResponseStream())
+			    {
+			        if (stream != null)
+			            using (var streamReader = new StreamReader(stream))
+			            {
+			                throw new PlayerIOError(ErrorCode.GeneralError, "Error communicating with the Player.IO WebService: " + streamReader.ReadToEnd());
+			            }
+			    }*/
+			}
+
+			return r;
+		}
