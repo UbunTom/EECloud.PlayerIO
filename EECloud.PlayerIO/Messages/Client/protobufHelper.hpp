557db56192604babfe8ae351c3972abf3b6a3172
UbunTom <thomas.collingwood@gmail.com>
2013-04-14 17:39:50 +0200

    Create protobufHelper.hpp

---
+++ EECloud.PlayerIO/Messages/Client/protobufHelper.hpp
@@ -0,0 +1,36 @@
+//These functions are used to convert a vector of KeyValuePair classes into a repeated sttribute of the socket protobuf class
+//The Socket class template should be a pointer to a class generated from a .proto file
+
+namespace EECloud
+{
+	template <class Socket>
+	void vector_RoomData(Socket socket,Vector<KeyValuePair>* plug)
+	{
+		socket.clear_RoomData();
+		for (var i=0;i<plug.size();i++)
+		{
+			socket.add_RoomData(plug[0]);
+		}
+	}
+
+	template <class Socket>
+	void vector_JoinData(Socket socket,Vector<KeyValuePair>* plug)
+	{
+		socket.clear_JoinData();
+		for (var i=0;i<plug.size();i++)
+		{
+			socket.add_JoinData(plug[0]);
+		}
+	}
+	
+	template <class Socket>
+	void vector_ExtraData(Socket socket,Vector<KeyValuePair>* plug)
+	{
+		socket.clear_ExtraData();
+		for (var i=0;i<plug.size();i++)
+		{
+			socket.add_ExtraData(plug[0]);
+		}
+	}
+
+}
