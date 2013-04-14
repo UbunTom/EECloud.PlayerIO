4c6d342588d79005fff130246d27d65e62adaa7c
UbunTom <thomas.collingwood@gmail.com>
2013-04-14 18:46:14 +0200

    Update Client.cpp

--- EECloud.PlayerIO/Client.cpp
+++ EECloud.PlayerIO/Client.cpp
@@ -1,3 +1,5 @@
+#include "Client.hpp"
+
 namespace EECloud
 {
 	Client::Client(HttpChannel* channel, string token, string connectUserId)
@@ -10,17 +12,18 @@ namespace EECloud
 	}
 
 
-	Connection* Client::CreateJoinRoom(string roomId, string serverType, bool visible = true, Dictionary<string, string> roomData = null, Dictionary<string, string> joinData = null)
+	Connection* Client::CreateJoinRoom(string roomId, string serverType, bool visible = true, map<string, string> roomData = null, map<string, string> joinData = null)
 	{
 		CreateJoinRoomArgs createJoinRoomArg = new CreateJoinRoomArgs;
 		createJoinRoomArg->set_RoomId(roomId);
 		createJoinRoomArg->set_ServerType(serverType);
 		createJoinRoomArg->set_Visible(visible);
-		createJoinRoomArg->set_RoomData(vector_RoomData<CreateJoinRoomArgs*>(Converter.Convert(roomData)));
-		createJoinRoomArg->set_JoinData(vector_JoinData<CreateJoinRoomArgs*>(Converter.Convert(joinData)));
-		createJoinRoomArg->set_IsDevRoom(DevelopmentServer != null);
+		vector_RoomData<CreateJoinRoomArgs*>(createJoinRoomArg,Converter.Convert(roomData));
+		vector_JoinData<CreateJoinRoomArgs*>(createJoinRoomArg,Converter.Convert(joinData));
+		createJoinRoomArg->set_IsDevRoom(DevelopmentServer != NULL);
 
 		CreateJoinRoomOutput* createJoinRoomOutput = _channel.Request<CreateJoinRoomArgs*, CreateJoinRoomOutput*, PlayerIOError*>(27, createJoinRoomArg);
+		delete createJoinRoomArg;
 		
 		ServerEndpoint* serverEndpoint;
 		if (DevelopmentServer == NULL) serverEndpoint = Converter.Convert(createJoinRoomOutput.Endpoints[0]);
@@ -29,19 +32,35 @@ namespace EECloud
 		return new Connection(serverEndpoint, createJoinRoomOutput.JoinKey);
 	}
 
-	Connection* Client::JoinRoom(string roomId, Dictionary<string, string> joinData = null)
+	Connection* Client::JoinRoom(string roomId, map<string, string> joinData = null)
 	{
-		var joinRoomArg = new JoinRoomArgs
-		{
-			RoomId = roomId,
-			JoinData = Converter.Convert(joinData),
-			IsDevRoom = DevelopmentServer != null
-		};
+		JoinRoomArgs* joinRoomArg = new JoinRoomArgs;
+		
+		joinRoomArg->set_RoomId(roomId);
+		vector_JoinData<JoinRoomArgs*>(joinRoomArg,Converter.Convert(joinData));
+		joinRoomArg->set_IsDevRoom(DevelopmentServer != NULL);
+		
 		JoinRoomOutput* joinRoomOutput = _channel.Request<JoinRoomArgs*, JoinRoomOutput*, PlayerIOError*>(24, joinRoomArg);
+		delete joinRoomArg;
 		
 		ServerEndpoint* serverEndpoint;
 		if (DevelopmentServer == NULL) serverEndpoint = Converter.Convert(createJoinRoomOutput.Endpoints[0]);
 		else serverEndpoint = DevelopmentServer;
 		
 		return new Connection(serverEndpoint, joinRoomOutput.JoinKey);
+	}
+	
+	
+	public Vector<RoomInfo> ListRooms(string roomType, map<string, string> searchCriteria = null, int resultLimit = 0, int resultOffset = 0, bool onlyDevRooms = false)
+	{
+		ListRoomsArgs* listRoomsArg = new ListRoomsArgs
+		
+		listRoomsArg->set_RoomType(roomType);
+		vector_JoinData<ListRoomsArgs*>(listRoomArg,Converter.Convert(searchCriteria));
+		listRoomsArg->set_ResultLimit(resultLimit);
+		listRoomsArg->set_ResultOffset(resultOffset);
+		listRoomsArg->set_OnlyDevRooms(onlyDevRooms);
+		
+		var listRoomsOutput = _channel.Request<ListRoomsArgs, ListRoomsOutput, PlayerIOError>(30, listRoomsArg);
+		return listRoomsOutput.RoomInfo ?? new RoomInfo[0];
 	}
