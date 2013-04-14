b84ff1b782d1095727ce2b1ceb154488e1459d76
UbunTom <thomas.collingwood@gmail.com>
2013-04-14 17:45:17 +0200

    Update Client.cpp

--- EECloud.PlayerIO/Client.cpp
+++ EECloud.PlayerIO/Client.cpp
@@ -12,19 +12,20 @@ namespace EECloud
 
 	Connection* Client::CreateJoinRoom(string roomId, string serverType, bool visible = true, Dictionary<string, string> roomData = null, Dictionary<string, string> joinData = null)
 	{
-		var createJoinRoomArg = new CreateJoinRoomArgs;
-		createJoinRoomArg.set_RoomId(roomId);
-		createJoinRoomArg.set_ServerType(serverType);
-		createJoinRoomArg.set_Visible(visible);
-		createJoinRoomArg.set_RoomData(Converter.Convert(roomData));
-		createJoinRoomArg.set_JoinData(Converter.Convert(joinData));
-		createJoinRoomArg.set_IsDevRoom(DevelopmentServer != null);
+		CreateJoinRoomArgs createJoinRoomArg = new CreateJoinRoomArgs;
+		createJoinRoomArg->set_RoomId(roomId);
+		createJoinRoomArg->set_ServerType(serverType);
+		createJoinRoomArg->set_Visible(visible);
+		createJoinRoomArg->set_RoomData(vector_RoomData<CreateJoinRoomArgs*>(Converter.Convert(roomData)));
+		createJoinRoomArg->set_JoinData(vector_JoinData<CreateJoinRoomArgs*>(Converter.Convert(joinData)));
+		createJoinRoomArg->set_IsDevRoom(DevelopmentServer != null);
 
-		var createJoinRoomOutput = _channel.Request<CreateJoinRoomArgs*, CreateJoinRoomOutput*, PlayerIOError*>(27, createJoinRoomArg);
+		CreateJoinRoomOutput* createJoinRoomOutput = _channel.Request<CreateJoinRoomArgs*, CreateJoinRoomOutput*, PlayerIOError*>(27, createJoinRoomArg);
 		
-		auto serverEndpoint;
+		ServerEndpoint* serverEndpoint;
 		if (DevelopmentServer == NULL) serverEndpoint = Converter.Convert(createJoinRoomOutput.Endpoints[0]);
-		else serverEndpoint = Converter.Convert(createJoinRoomOutput.Endpoints[0]);
+		else serverEndpoint = DevelopmentServer;
+		
 		return new Connection(serverEndpoint, createJoinRoomOutput.JoinKey);
 	}
 
@@ -36,7 +37,11 @@ namespace EECloud
 			JoinData = Converter.Convert(joinData),
 			IsDevRoom = DevelopmentServer != null
 		};
-		var joinRoomOutput = _channel.Request<JoinRoomArgs, JoinRoomOutput, PlayerIOError>(24, joinRoomArg);
-		var serverEndpoint = DevelopmentServer ?? Converter.Convert(joinRoomOutput.Endpoints[0]);
+		JoinRoomOutput* joinRoomOutput = _channel.Request<JoinRoomArgs*, JoinRoomOutput*, PlayerIOError*>(24, joinRoomArg);
+		
+		ServerEndpoint* serverEndpoint;
+		if (DevelopmentServer == NULL) serverEndpoint = Converter.Convert(createJoinRoomOutput.Endpoints[0]);
+		else serverEndpoint = DevelopmentServer;
+		
 		return new Connection(serverEndpoint, joinRoomOutput.JoinKey);
 	}
